6c67514028a152a2a333845edfb5b89c935ee360
Orangeify <jairema.andrade@gmail.com>
2025-11-21 07:14:39 -0600

    clocked in

---
+++ public/assets/js/clocked-fps-life.js
@@ -0,0 +1,29 @@
+class DigitalClock {
+    constructor(element) {
+        this.element = element;
+    }
+
+    update() {
+        const parts = this.getTimeParts();
+        const minuteFormatted = parts.minute.toString().padStart(2, '0');
+        const secondFormatted = parts.second.toString().padStart(2, '0');
+        const timeFormatted = `${parts.hour}:${minuteFormatted}:${secondFormatted}`;
+
+        console.log(timeFormatted);
+
+        this.element.querySelector('.clock-time').textContent = timeFormatted;
+    }
+        
+
+    getTimeParts() {
+        const now = new Date();
+        return {
+            hour: now.getHours() % 12 || 12,
+            minute: now.getMinutes(),
+            second: now.getSeconds(),
+        };
+    }
+}
+
+const clockElement = document.querySelector('.clock-time');
+const clockObject = new DigitalClock(clockElement);
