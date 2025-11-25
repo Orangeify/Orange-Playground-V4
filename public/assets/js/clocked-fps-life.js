830bf160fccdab5fd5ba1803c993de56d390c866
Orangeify <jairema.andrade@gmail.com>
2025-11-24 21:54:55 -0600

    this won't work

--- public/assets/js/clocked-fps-life.js
+++ public/assets/js/clocked-fps-life.js
@@ -60,8 +60,15 @@ clockObject.start();
 
 const chargeLevel = document.querySelector('.battery-percentage');
 const chargeBar = document.querySelector('.battery-level');
+const chargeStatus = document.getElementById('charging-icon');
 
 function updateBatteryStatus(battery) {
+     if (battery.charging === true) {
+        chargeStatus.classList.remove = ".dnone";
+    } else {
+        chargeStatus.classList.add = ".dnone";
+    }
+
     const chargePercentage = battery.level * 100;
     chargeLevel.textContent = chargePercentage + "%";
     chargeBar.style.width = chargePercentage + "%";
