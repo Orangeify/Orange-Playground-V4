fa5f747845bf56f10ca505c57bb0a5d5f117a20b
Orangeify <jairema.andrade@gmail.com>
2025-12-14 17:16:33 -0600

    shitty squid game

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 //Import express, http and wisp
 import http from 'node:http';
 import express from 'express';
-import wisp from 'wisp-server-node';
+import { server as wisp, logging } from "@mercuryworkshop/wisp-js/server";
 import path from 'node:path';
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
 import { baremuxPath } from "@mercuryworkshop/bare-mux/node"; //Note how we are using /node at the end of this import. This provides the correct types when using TypeScript.
@@ -10,7 +10,7 @@ import { epoxyPath } from "@mercuryworkshop/epoxy-transport";
 //create the express "app"
 const app = express();
 //create an http server
-const httpServer = http.createServer();
+const server = http.createServer();
 //define the port to listen on
 //change this to your liking!
 const port = 8080;
@@ -26,14 +26,16 @@ app.use("/baremux/", express.static(baremuxPath));
 // "/epoxy/" is where the epoxy files will be served from. epoxyPath is just the location to those files.
 app.use("/epoxy/", express.static(epoxyPath));
 
+logging.set_level(logging.NONE); //set wisp logging to none. Change to logging.INFO or logging.DEBUG for more verbose logging.
+
 //listen for requests on the http server.
-httpServer.on('request', (req, res) => {
+server.on('request', (req, res) => {
     //make express handle all of the requests
     app(req, res)
 });
 
 //listen for websocket upgrades on the http server
-httpServer.on('upgrade', (req, socket, head) => {
+server.on('upgrade', (req, socket, head) => {
     if (req.url.endsWith('/wisp/')) {
         //route the request to the wisp server if the url ends in /wisp/
         wisp.routeRequest(req, socket, head);
@@ -44,12 +46,12 @@ httpServer.on('upgrade', (req, socket, head) => {
 });
 
 //when the server is ready, console.log that it is ready
-httpServer.on('listening', () => {
+server.on('listening', () => {
     console.log(`Server listening on http://localhost:${port}`);
 });
 
 //start the http server
-httpServer.listen({
+server.listen({
     port: port 
 });
 
