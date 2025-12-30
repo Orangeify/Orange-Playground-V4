72288253ff1810b2b93c909db5773010ef8e987d
Orangeify <jairema.andrade@gmail.com>
2025-12-30 17:50:51 -0600

    balls???

---
+++ public/assets/js/theme-selector.js
@@ -0,0 +1,17 @@
+function initThemeSelector() {
+    const themeSelect = document.getElementById('themeSelect');
+    const themeStylesheetLink = document.getElementById('themeStylesheetLink');
+    const savedTheme = localStorage.getItem('theme') || 'original-orange';
+
+    function applyTheme(themeName) {
+        themeStylesheetLink.setAttribute('href', `/assets/css/themes/${themeName}.css`);
+    }
+
+    themeSelect.addEventListener('change', function() {
+        applyTheme(themeSelect.value);
+        localStorage.setItem('theme', themeSelect.value);
+    });
+
+    themeSelect.value = savedTheme;
+    applyTheme(savedTheme);
+}
