2b3826fa57bd00959ff3b30512ba60915477540c
Orangeify <jairema.andrade@gmail.com>
2025-12-31 18:08:01 -0600

    minor change

--- public/assets/js/particle-toggle.js
+++ public/assets/js/particle-toggle.js
@@ -2,7 +2,7 @@ const particleToggle = document.getElementById('particles-js');
 const sw = document.getElementById('particle-checkbox');
 
 sw.addEventListener('change', function() {
-    if (sw.checked) {
+    if (sw.checked === false) {
         particleToggle.classList.add("dnone")
     } else {
         particleToggle.classList.remove("dnone")
