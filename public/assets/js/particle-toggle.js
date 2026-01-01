0a0437fa52820b5f57c6ad261dac8fbfefb2124a
Orangeify <jairema.andrade@gmail.com>
2025-12-31 18:36:08 -0600

    why tf

--- public/assets/js/particle-toggle.js
+++ public/assets/js/particle-toggle.js
@@ -2,7 +2,9 @@ const particleToggle = document.getElementById('particles-js');
 const sw = document.getElementById('particle-checkbox');
 
 const saved = localStorage.getItem('particleSetting') || enabled;
-    // If switch is ON, run launcher immediately on page load
+    const disabled = sw.checked === false;
+    const enabled = sw.checked;
+// If switch is ON, run launcher immediately on page load
     if(saved === disabled){
       particleToggle.classList.add("dnone");
     }
@@ -10,11 +12,9 @@ const saved = localStorage.getItem('particleSetting') || enabled;
 sw.addEventListener('change', function() {
     if (sw.checked === false) {
         particleToggle.classList.add("dnone")
-        const disabled = 'sw.checked === false';
         localStorage.setItem('particleSetting', disabled);
     } else {
         particleToggle.classList.remove("dnone")
-        const enabled = 'sw.checked';
         localStorage.setItem('particleSetting', enabled)
     }
 });
