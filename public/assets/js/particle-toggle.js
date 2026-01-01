66ef3b0b02e79ac5a8f9d7c15d13ce7f0e1e39b6
Orangeify <jairema.andrade@gmail.com>
2025-12-31 18:54:50 -0600

    longhorn

--- public/assets/js/particle-toggle.js
+++ public/assets/js/particle-toggle.js
@@ -1,7 +1,7 @@
 const particleToggle = document.getElementById('particles-js');
 const sw = document.getElementById('particle-checkbox');
 
-const saved = localStorage.getItem('particleSetting') || true;
+const saved = localStorage.getItem('particleSetting');
 // If switch is ON, run launcher immediately on page load
     if(saved === false){
       particleToggle.classList.add("dnone");
