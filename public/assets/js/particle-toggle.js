240ac49f9bd31406acf548bacba9705146419914
Orangeify <jairema.andrade@gmail.com>
2025-12-31 18:22:01 -0600

    broken script

--- public/assets/js/particle-toggle.js
+++ public/assets/js/particle-toggle.js
@@ -1,9 +1,19 @@
 const particleToggle = document.getElementById('particles-js');
 const sw = document.getElementById('particle-checkbox');
 
+const saved = localStorage.getItem('particleSetting') === 'true';
+sw.checked = saved;
+
+    // If switch is ON, run launcher immediately on page load
+    if(saved){
+      particleToggle.classList.add("dnone");
+    }
+
 sw.addEventListener('change', function() {
     if (sw.checked === false) {
         particleToggle.classList.add("dnone")
+        const disabled = sw.checked === false;
+        localStorage.setItem('particleSetting', disabled);
     } else {
         particleToggle.classList.remove("dnone")
     }
