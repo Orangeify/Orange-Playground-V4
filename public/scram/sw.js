813633b36df319a434a18009b2ba9d9a6dc2dba2
Orangeify <jairema.andrade@gmail.com>
2025-12-31 21:49:54 -0600

    remodel

--- public/scram/sw.js
+++ public/scram/sw.js
@@ -3,13 +3,14 @@ importScripts("/scram/scramjet.all.js");
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
 
-self.addEventListener("fetch", (event) => {
-	event.respondWith(async () => {
-    await scramjet.loadConfig();
-    if (scramjet.route(event)) {
-      return scramjet.fetch(event);
-    }
+async function handleRequest(event) {
+	await scramjet.loadConfig();
+	if (scramjet.route(event)) {
+		return scramjet.fetch(event);
+	}
+	return fetch(event.request);
+}
 
-    return fetch(event.request);
-  });
+self.addEventListener("fetch", (event) => {
+	event.respondWith(handleRequest(event));
 });
