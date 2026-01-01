f0a379e48099a297944508f6723ae4ce425f80ce
Orangeify <jairema.andrade@gmail.com>
2025-12-31 18:04:11 -0600

    simple switch

--- public/assets/js/particle-toggle.js
+++ public/assets/js/particle-toggle.js
@@ -1,3 +1,11 @@
 const particleToggle = document.getElementsById('particles-js');
 const sw = document.getElementById('particle-checkbox');
 
+sw.addEventListener('change', function() {
+    if (sw.checked) {
+        particleToggle.classList.add("dnone")
+    } else {
+        particleToggle.classList.remove("dnone")
+    }
+});
+
